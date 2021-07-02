0520cbad8c9ad212eab80426ac81aef000978c3d
schriverdj87 <schriverdj87@gmail.com>
2021-07-02 16:31:46 -0300

    First commit

---
+++ Demo_Sound/Assets/MainGameCore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//The main class of the "game" 
+public class MainGameCore : MonoBehaviour
+{
+    SoundWranglerCore swc;
+
+    private void Start()
+    {
+        swc = FindObjectOfType<SoundWranglerCore>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //If they press F the sound plays
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            swc.PlayTheSound();
+        }
+    }
+}
---
+++ Demo_Sound/Assets/SoundWranglerCore.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+// A sort of "box" 
+public class SoundWranglerCore : MonoBehaviour
+{
+    //This is set through the interface
+    [SerializeField] public AudioClip sound;
+    private AudioSource sauce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Use this method to instantiate a sauce
+        sauce = gameObject.AddComponent<AudioSource>();
+        //Plug the AudioClip into the source
+        sauce.clip = sound;
+    }
+
+    public void PlayTheSound()
+    {
+        sauce.Play();
+    }
+}
